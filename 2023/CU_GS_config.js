45fe60d0aaea8ba4d8b384d7e0bb427ea37b3945
Modular-Holmes <77898901+Modular-Holmes@users.noreply.github.com>
2023-03-24 19:18:22 -0400

    Update CU_GS_config.js gbos

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2023norshore",
+      "defaultValue": "2023gbos",
       "required": "true",
       "disabled": "true"
     },
